2e8c6b5c4d4df3085e69e7ca7278d2a8e6215035
Jose colina <jose.colina@jalasoft.com>
2019-11-26 21:24:39 -0400

    Adding base files

---
+++ tests/data_structures/MyLinkedListTest.java
@@ -0,0 +1,15 @@
+package data_structures;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNotEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertNull;
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+public class MyLinkedListTest {
+    @Test
+    
+}
